e5509926fd9959dcce65217c61f8a9c0c517db5c
Dheiver Francisco Santos <165256830+dheiver2@users.noreply.github.com>
2025-02-28 18:17:05 -0300

    Create SettingsTab.js

---
+++ SettingsTab.js
@@ -0,0 +1,12 @@
+export default function SettingsTab() {
+  return (
+    <div>
+      <h2>Planos de Assinatura</h2>
+      <div>
+        <p>Plano Gratuito: Análise básica com limite diário.</p>
+        <p>Plano PRO (R$ 19,90/mês): Mais detalhes nutricionais.</p>
+        <p>Plano PREMIUM (R$ 49,90/mês): Análises ilimitadas.</p>
+      </div>
+    </div>
+  );
+}
